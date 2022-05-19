7c505e0d67b6cbad91719643e4b8f6cb36d72927
Nam Hoang Nguyen <nguyen6626nam@gmail.com>
2022-05-19 10:49:08 +0700

    First commit

---
+++ expanding-nebula/test.py
@@ -0,0 +1,32 @@
+def test():
+    from main import solution
+
+    input = [[True, False, True], [False, True, False], [True, False, True]]
+    output = solution(input)
+    print(output)
+    assert output == 4
+
+    input = [
+        [True, True, False, True, False, True, False, True, True, False],
+        [True, True, False, False, False, False, True, True, True, False],
+        [True, True, False, False, False, False, False, False, False, True],
+        [False, True, False, False, False, False, True, True, False, False],
+    ]
+    output = solution(input)
+    print(output)
+    assert output == 11567
+
+    input = [
+        [True, False, True, False, False, True, True, True],
+        [True, False, True, False, False, False, True, False],
+        [True, True, True, False, False, False, True, False],
+        [True, False, True, False, False, False, True, False],
+        [True, False, True, False, False, True, True, True],
+    ]
+    output = solution(input)
+    print(output)
+    assert output == 254
+
+
+test()
+# print(sum((True, False, True)))
