4a901c17758098f8cffb737568278163aac1885f
Nam Hoang Nguyen <nguyen6626nam@gmail.com>
2022-05-24 10:29:33 +0700

    Build by column, based on last column

--- expanding-nebula/main.py
+++ expanding-nebula/main.py
@@ -4,7 +4,9 @@ from itertools import product
 
 def solution(g):
     def new_cell_config_table():
-        return tuple(tuple(product((False, True), repeat=repeat)) for repeat in (4, 2, 1))
+        return tuple(
+            tuple(product((False, True), repeat=repeat)) for repeat in (4, 2, 1)
+        )
 
     def new_kernel_sum_table():
         return {kernel: sum(kernel) == 1 for kernel in product((False, True), repeat=4)}
@@ -31,14 +33,27 @@ def solution(g):
 
     def valid_columns(previous_col_half, col_index):
         for config0 in cell_config_table[1]:
-            if g[0][col_index] != kernel_sum_table[config0 + previous_col_half[0] + previous_col_half[1]]:
+            if (
+                g[0][col_index]
+                != kernel_sum_table[
+                    config0 + previous_col_half[0] + previous_col_half[1]
+                ]
+            ):
                 continue
-            col = [previous_col_half[0] + config0[:1], previous_col_half[1] + config0[1:]]
+            col = [
+                previous_col_half[0] + config0[:1],
+                previous_col_half[1] + config0[1:],
+            ]
             col.extend(half for half in previous_col_half[2:])
             cell_stack = [(1, config) for config in cell_config_table[2]]
             while cell_stack:
                 row_index, config = cell_stack.pop()
-                if g[row_index][col_index] != kernel_sum_table[col[row_index] + col[row_index + 1][:1] + config]:
+                if (
+                    g[row_index][col_index]
+                    != kernel_sum_table[
+                        col[row_index] + col[row_index + 1][:1] + config
+                    ]
+                ):
                     continue
                 col[row_index + 1] = col[row_index + 1][:1] + config
                 if row_index == len(g) - 1:
