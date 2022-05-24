d3b80be8da62b2d47446170ebe2f421740bbb748
Nam Hoang Nguyen <nguyen6626nam@gmail.com>
2022-05-24 10:05:47 +0700

    Build column from last column

--- expanding-nebula/main.py
+++ expanding-nebula/main.py
@@ -6,21 +6,21 @@ def solution(g):
     def new_cell_config_table():
         return tuple(product((False, True), repeat=4)), tuple(
             product((False, True), repeat=2)
-        )
+        ), tuple(product((False, True), repeat=1))
 
     def new_kernel_sum_table():
         return {kernel: sum(kernel) == 1 for kernel in product((False, True), repeat=4)}
 
-    def valid_columns(col_index):
+    def valid_base_column():
         for config0 in cell_config_table[0]:
-            if g[0][col_index] != kernel_sum_table[config0]:
+            if g[0][0] != kernel_sum_table[config0]:
                 continue
             col = [config0[:2], config0[2:]]
             col.extend(None for _ in range(len(g) - 1))
             cell_stack = [(1, config) for config in cell_config_table[1]]
             while cell_stack:
                 row_index, config = cell_stack.pop()
-                if g[row_index][col_index] != kernel_sum_table[col[row_index] + config]:
+                if g[row_index][0] != kernel_sum_table[col[row_index] + config]:
                     continue
                 col[row_index + 1] = config
                 if row_index == len(g) - 1:
@@ -31,6 +31,26 @@ def solution(g):
                         for next_config in cell_config_table[1]
                     )
 
+    def valid_columns(previous_col_half, col_index):
+        for config0 in cell_config_table[1]:
+            if g[0][col_index] != kernel_sum_table[config0 + previous_col_half[0] + previous_col_half[1]]:
+                continue
+            col = [previous_col_half[0] + config0[:1], previous_col_half[1] + config0[1:]]
+            col.extend(half for half in previous_col_half[2:])
+            cell_stack = [(1, config) for config in cell_config_table[2]]
+            while cell_stack:
+                row_index, config = cell_stack.pop()
+                if g[row_index][col_index] != kernel_sum_table[col[row_index] + col[row_index + 1][:1] + config]:
+                    continue
+                col[row_index + 1] = col[row_index + 1][:1] + config
+                if row_index == len(g) - 1:
+                    yield tuple(col)
+                if row_index < len(g) - 1:
+                    cell_stack.extend(
+                        (row_index + 1, next_config)
+                        for next_config in cell_config_table[2]
+                    )
+
     def column_match(col1, col2):
         for row1, row2 in zip(col1, col2):
             if row1[1] != row2[0]:
@@ -38,7 +58,8 @@ def solution(g):
         return True
 
     def build_by_col(col_index, col):
-        for next_col in valid_columns(col_index + 1):
+        previous_col_half = tuple(row[1:] for row in col)
+        for next_col in valid_columns(previous_col_half, col_index + 1):
             if column_match(col, next_col):
                 config_count[col_index][col] += (
                     1
@@ -52,6 +73,6 @@ def solution(g):
     config_count = [defaultdict(int) for _ in range(len(g[0]) - 1)]
     cell_config_table = new_cell_config_table()
     kernel_sum_table = new_kernel_sum_table()
-    for column in valid_columns(0):
+    for column in valid_base_column():
         build_by_col(0, column)
     return sum(config_count[0][config] for config in config_count[0])
